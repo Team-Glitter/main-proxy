b19edb4230cdb2f72f9e157285892ab380949772
Kim Luu <kimluuwdev@gmail.com>
2020-08-21 00:10:25 -0700

    add css for the whole page

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path')
-
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(morgan('dev'));
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('')
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname, './client/dist')));
 app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
